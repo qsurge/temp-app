3d001b9bd0af1f4e31d2570f9171580cbc11240c
sairajag <sairaja.gdepu@500apps.com>
2023-08-01 17:26:30 +0530

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import path from 'path';
+
+export default defineNuxtConfig({
+  app: {
+    baseURL: "/temp-app/",
+  },
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, "docs"),
+    },
+  },
+});
